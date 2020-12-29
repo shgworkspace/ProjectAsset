480f8472fe2a8e3e7c206609747e930afb3d12bf
shgworkspace <shg.workspace@sukosolhotels.com>
2020-12-29 12:17:00 +0700

    up001

--- myvue/test/t1/src/App.vue
+++ myvue/test/t1/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App55555"/>
+    <HelloWorld msg="Welcome to Your Vue.js App789987"/>
   </div>
 </template>
 
